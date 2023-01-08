8a2ef5b764618733af11900536f89404256c1ac7
Kyle Coulon <kylejcoulon@gmail.com>
2023-01-07 17:05:49 -0700

    clean up

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,14 @@ const controllers_path = path.join(__dirname, '../controllers');
 // Require controller modules.
 const home_controller = require(path.join(controllers_path, "homeController"));
 const test_controller = require(path.join(controllers_path, "testController"));
-const user_controller = require(path.join(controllers_path, "userController"));
+
+// API
 const contacts_controller = require(path.join(controllers_path, "contactsController"));
-
-
-routes.get('/', user_controller.getUser);
-routes.get('/username', user_controller.getUsername);
 
 routes.get('/contacts', contacts_controller.getContacts);
 routes.get('/contact', contacts_controller.getContact);
 
-routes.get('/hoe', home_controller.index);
+routes.get('/', home_controller.index);
 routes.get('/home/people', home_controller.index_people_list);
 routes.get('/home/sign-in', home_controller.index_sign_in);
 
